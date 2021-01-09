29af5f399f2b950789aefdeb76aae3f8692f80a2
Savannah Sew-Hee <savrosealbright@gmail.com>
2021-01-09 13:58:08 -0600

    added sunburst

--- ui.R
+++ ui.R
@@ -5,7 +5,8 @@ shinyUI(navbarPage(title = "Art Museum Project", inverse = TRUE,
                    # ********** 0. ABOUT TAB **********
                    tabPanel("ABOUT", 
                             sidebarPanel(
-                              selectInput("decade_selection", "Choose a decade:",
+                              selectInput("decade_selection",
+                                          "Choose a decade:",
                                           choices = c("1930s" = "1",
                                                       "1940s" = "2",
                                                       "1950s" = "3",
@@ -15,47 +16,41 @@ shinyUI(navbarPage(title = "Art Museum Project", inverse = TRUE,
                                                       "1990s" = "7",
                                                       "2000s" = "8",
                                                       "2010s" = "9")),
-                              
-                              #actionButton("update", "Change"),
                               hr(),
                               sliderInput("freq",
                                           "Minimum Frequency:",
-                                          min = 1,  max = 50, value = 15),
+                                          min = 1,  max = 50, value = 20),
                               sliderInput("max",
                                           "Maximum Number of Words:",
-                                          min = 1,  max = 300,  value = 100)
+                                          min = 1,  max = 300,  value = 200)
                             ),
                             
                             # Show Word Cloud
                             mainPanel(
-                              plotOutput("wordcloud_plot",width = "500px", height="500px")
+                              h2("  Top Mediums Used by Decade"),
+                              plotOutput("wordcloud_plot",width = "500px", height="400px")
                             )
                    ),
                    
                    
                    
                    # ********** 1. DISCREPANCIES IN THE DATA TAB **********
-                   tabPanel("DISCREPANCIES IN DATA",
-                            sidebarLayout(
-                                sidebarPanel(
-                                    h2('Hi... text here')
-                                ),
-                                mainPanel(
-                                    h2('Hi... text here')
-                                )
-                            )    
-                   ),
+                  # tabPanel("DISCREPANCIES IN DATA",
+                            #sidebarLayout(
+                             #   sidebarPanel(
+                              #      h2('Hi... text here')
+                               # ),
+                          #      mainPanel(
+                           #         h2('Hi... text here')
+                            #    )
+                            #)    
+                   #),
                    
                    
                    
               
                    # ********** 2. DIVERSITY OF ARTISTS AND ARTWORKS TAB **********
                    tabPanel("DIVERSITY OF ARTISTS AND ARTWORKS",
-                            sidebarLayout(
-                                sidebarPanel(
-                                    h2('put text here')
-                                ),
-                                mainPanel(
                                   tabsetPanel(type = "tabs",
                                               tabPanel("Artists", 
                                                        plotOutput("Gender_Pie"),
@@ -64,14 +59,12 @@ shinyUI(navbarPage(title = "Art Museum Project", inverse = TRUE,
                                               tabPanel("Artworks", 
                                                        h2('categories'))
                                   )
-                                )
-                            )    
                    ),
                    
                    
                    
                    # ********** 3. TIME SERIES DATA TAB **********
-                   tabPanel("TIME SERIES",
+                   tabPanel("Data Over Time",
                             sidebarLayout(
                                 sidebarPanel(
                                   checkboxGroupInput("Dep_Input", "Classification",
@@ -86,7 +79,6 @@ shinyUI(navbarPage(title = "Art Museum Project", inverse = TRUE,
                                   
                                 ),
                                 mainPanel(
-                                  textOutput("txt"),
                                   plotOutput("Dep_plot")
                                 )
                             )    
